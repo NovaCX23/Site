842a2e33e3c2f62cd12c42fd6c3ab6cab44352cb
Chris <xhamanichristian@gmail.com>
2024-11-18 11:08:14 +0200

    done with faqs

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-// change navbar style on scroll
+// Change navbar style on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+});
 
-window.addEventListener('scroll', () =>{
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
-})
+// Show/hide FAQ and toggle icon
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // Change icon inside the event listener for each FAQ click
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.classList.contains('uil-plus')) {
+            icon.classList.remove('uil-plus');
+            icon.classList.add('uil-minus');
+        } else {
+            icon.classList.remove('uil-minus');
+            icon.classList.add('uil-plus');
+        }
+    });
+});
