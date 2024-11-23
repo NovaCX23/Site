f50cd6bc66de0e00b5002dffbe511a1b5570a96f
Chris <xhamanichristian@gmail.com>
2024-11-23 17:19:10 +0200

    hehe merge si romana

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+      pageLanguage: 'en',
+      includedLanguages: 'en,ro'
+    }, 'google_translate_element');
+}
+
 // Change navbar style on scroll
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
